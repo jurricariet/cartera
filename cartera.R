4db18f7034f522700b582d6e406d2c51aba9631d
jurricariet <juanurricariet@gmail.com>
2024-06-06 11:45:01 -0300

    grafico

--- cartera.R
+++ cartera.R
@@ -1,6 +1,7 @@
 library(yfR)
 library(tidyverse)
 library(lubridate)
+library(directlabels)
 # BONOS: descarga desde Alphacast
 ticker_bono <- 'GD35'
 bonos <- read_csv('https://api.alphacast.io/datasets/5357/data?apiKey=ak_b4clHOA8NEK7Gv5q5S5R&$format=csv') %>% 
@@ -12,7 +13,7 @@ df_bonos <- bonos %>%
 # CEDEARS desde el paquete yahoo finance
 # Buscar tickers en https://finance.yahoo.com
 
-ticker_acciones <- c('CVX.BA','INTC.BA')
+ticker_acciones <- c('CVX.BA','INTC')
 first_date <- as.Date('2024-01-01')
 last_date <- Sys.Date()
 
@@ -40,11 +41,98 @@ fecha_compra = as.Date('2024-05-28')
 df_todos <- bind_rows(df_bonos,df_acciones) %>% 
   bind_rows(df_mep) %>%  
   group_by(ticker) %>% 
-  mutate(indice_100 = 100*precio/precio[fecha==fecha_compra])
+  mutate(indice_100 = 100*precio/precio[fecha==fecha_compra]) %>% 
+  group_by(fecha) %>% 
+  mutate(vs_mep = precio/precio[ticker == 'MEP'])
 
 df_todos %>% 
   filter(fecha >= fecha_compra) %>% 
   ggplot(aes(fecha,y=indice_100,group=ticker,color=ticker))+
+  geom_dl(aes(label = ticker), method = list(dl.combine( "last.points")), cex = 0.8)+ 
   geom_line()+
   geom_point()+
-  geom_hline(yintercept=100,color='black')
+  geom_hline(yintercept=100,color='black')+
+  ggthemes::scale_color_canva()+
+  theme_minimal()+
+  theme(legend.position='none')+
+  labs(x='',y='',title='Evolución activos',
+       subtitle='Índice 28-may = 100')
+
+
+df_todos %>% 
+  filter(fecha >= fecha_compra) %>% 
+  ggplot(aes(fecha,y=vs_mep,group=ticker,color=ticker))+
+  geom_dl(aes(label = ticker), method = list(dl.combine( "last.points")), cex = 0.8)+ 
+  geom_line()+
+  geom_point()+
+  geom_hline(yintercept=1,color='black')+
+  ggthemes::scale_color_canva()+
+  theme_minimal()+
+  theme(legend.position='none')+
+  labs(x='',y='',title='Evolución activos',
+       subtitle='Índice 28-may = 100')
+
+####
+datapasta::df_paste()
+cvx.ba <- data.frame(
+  stringsAsFactors = FALSE,
+  Fecha.Cotización = c("6/5/2024","6/4/2024",
+                       "6/3/2024","05/31/2024","05/30/2024","05/29/2024",
+                       "05/28/2024"),
+          Apertura = c(12900, 12785, 12659.5, 12100, 11900, 12319, 12513),
+            Máximo = c(13140, 13050, 13100, 12709, 12066, 12319, 12775.5),
+            Mínimo = c(12623.5, 12100, 12382.5, 12042, 11700, 11701.5, 12160),
+            Cierre = c(12658, 12819, 12731, 12697.5, 12060, 11922, 12313),
+   Cierre.ajustado = c(12658, 12819, 12731, 12697.5, 12060, 11922, 12313),
+     Volumen.Monto = c(226223344,151861057.5,
+                       111599772,138718810,181903413,139774967.5,121816192.5),
+   Volumen.Nominal = c(17767, 11887, 8815, 11115, 15276, 11814, 9959)
+) %>% 
+  mutate(fecha = mdy(Fecha.Cotización),
+         precio=Cierre.ajustado,
+         ticker = 'CVX.BA') %>% 
+  select(fecha,precio,ticker)
+
+datapasta::df_paste()
+intc.ba <- data.frame(
+  stringsAsFactors = FALSE,
+  Fecha.Cotización = c("6/5/2024","6/4/2024",
+                       "6/3/2024","05/31/2024","05/30/2024","05/29/2024",
+                       "05/28/2024"),
+          Apertura = c(7960, 7860, 7650, 7497, 7345, 7635, 7874.5),
+            Máximo = c(8200, 8149.5, 7897.5, 7742, 7396.5, 7635, 7874.5),
+            Mínimo = c(7900, 7788, 7591, 7339, 7000, 7300, 7560),
+            Cierre = c(8050, 7868, 7826, 7700, 7352, 7340, 7720.5),
+   Cierre.ajustado = c(8050, 7868, 7826, 7700, 7352, 7340, 7720.5),
+     Volumen.Monto = c(152505633,275841296.5,
+                       168342362.5,146628157,216726555,124353955.5,212246265.5),
+   Volumen.Nominal = c(19051, 34895, 21686, 19536, 29571, 16911, 27705)
+) %>% 
+  mutate(fecha = mdy(Fecha.Cotización),
+         precio=Cierre.ajustado,
+         ticker = 'INTC.BA') %>% 
+  select(fecha,precio,ticker)
+
+
+df_todos <- bind_rows(df_bonos) %>% 
+  bind_rows(df_mep) %>%  
+  bind_rows(intc.ba) %>% 
+  bind_rows(cvx.ba) %>% 
+  group_by(ticker) %>% 
+  mutate(indice_100 = 100*precio/precio[fecha==fecha_compra]) %>% 
+  group_by(fecha) %>% 
+  mutate(vs_mep = precio/precio[ticker == 'MEP'])
+
+df_todos %>% 
+  filter(fecha >= fecha_compra) %>% 
+  ggplot(aes(fecha,y=indice_100,group=ticker,color=ticker))+
+  geom_dl(aes(label = ticker), method = list(dl.combine( "last.points")), cex = 0.8)+ 
+  geom_line()+
+  geom_point()+
+  geom_hline(yintercept=100,color='black')+
+  ggthemes::scale_color_canva()+
+  theme_minimal()+
+  theme(legend.position='none')+
+  labs(x='',y='',title='Evolución activos',
+       subtitle='Índice 28-may = 100')
+ggsave('evolucion.png',scale=2)
